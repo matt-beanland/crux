9acc411fca1b0b7d236a64549b798e5d2e2f1598
Matt Beanland <beanland@live.com.au>
2026-02-03 20:20:35 +1030

    accumlating unique and counting

--- lib/crux/expression/rewrite_rule/idempotent_law.ex
+++ lib/crux/expression/rewrite_rule/idempotent_law.ex
@@ -21,20 +21,18 @@ defmodule Crux.Expression.RewriteRule.IdempotentLaw do
 
   @impl Crux.Expression.RewriteRule
   def walk({op, left, right}) do
-    list =
-      left
-      |> gather(op)
-      |> Enum.concat(gather(right, op))
+    # Build list in order while avoiding O(n²) concatenation by using accumulators
+    list = gather(left, op, gather(right, op, []))
 
-    uniq =
-      Enum.uniq(list)
+    # Deduplicate in O(n) instead of O(n²) while preserving first-occurrence order
+    {uniq, count} = dedup_with_count(list)
 
     case uniq do
       [single] ->
         single
 
       multiple ->
-        if Enum.count(list) == Enum.count(uniq) do
+        if count == length(uniq) do
           {op, left, right}
         else
           Enum.reduce(multiple, &{op, &2, &1})
@@ -44,9 +42,30 @@ defmodule Crux.Expression.RewriteRule.IdempotentLaw do
 
   def walk(other), do: other
 
-  defp gather({op, left, right}, op) do
-    gather(left, op) ++ gather(right, op)
+  # Gather elements using continuation-passing style to avoid list concatenation
+  # Builds list from right-to-left, so we gather right first, then left
+  defp gather({op, left, right}, op, cont) do
+    gather(left, op, gather(right, op, cont))
   end
 
-  defp gather(other, _), do: [other]
+  defp gather(other, _op, cont) do
+    [other | cont]
+  end
+
+  # Remove duplicates in O(n) while preserving order and counting originals
+  defp dedup_with_count(list) do
+    {uniq_reversed, _seen, count} =
+      Enum.reduce(list, {[], MapSet.new(), 0}, fn item, {acc, seen, count} ->
+        if MapSet.member?(seen, item) do
+          # Duplicate found - don't add to result but increment count
+          {acc, seen, count + 1}
+        else
+          # First occurrence - add to result and seen set
+          {[item | acc], MapSet.put(seen, item), count + 1}
+        end
+      end)
+
+    # Reverse to get first-occurrence order
+    {Enum.reverse(uniq_reversed), count}
+  end
 end
